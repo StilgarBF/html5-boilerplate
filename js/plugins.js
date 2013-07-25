827026f88882947ac386e4ce27f66c90727c4972
Tino Dietel <tino.dietel@gmail.com>
2013-07-25 16:56:00 +0200

    boilerplate for jQuery plugins added

--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,42 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+/**
+ * plugin boilerplate
+ *
+ * options:
+ *
+ * @return {jQuery Object}
+ */
+(function( $ ){
+    var defaults = {};
+    var settings;
+
+    var methods = {
+        init : function( options )
+        {
+            // build settings-object from defaults
+            settings = $.extend({}, defaults, options);
+
+            return this.each(function()
+            {
+                var $this = $(this);
+            });
+        },
+        etc : function( )
+        {
+        }
+    };
+
+    $.fn.fooPlugin = function( method )
+    {
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.fooPlugin' );
+        }
+    };
+})( jQuery );
